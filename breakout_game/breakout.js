1b2c976904d7ada39b4ba49888c7dff5990cdccc
avjmachine <35830668+avjmachine@users.noreply.github.com>
2022-06-28 01:14:06 +0530

    add paddle and restrict its movement

--- breakout_game/breakout.js
+++ breakout_game/breakout.js
@@ -8,6 +8,14 @@ let y = canvas.height - 25;
 let dx = 2;
 let dy = -2;
 
+var paddleHeight = 15;
+var paddleWidth = 80;
+var paddleX = (canvas.width - paddleWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
+
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false);
@@ -16,20 +24,63 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
-    if (y + dy < ballRadius || y + dy > canvas.height-ballRadius) {
+    if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
         dy = -dy;
     }
 
-    if (x + dx < ballRadius || x + dx > canvas.width-ballRadius) {
+    if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx;
     }
 
     x += dx;
     y += dy;
+
+    if (rightPressed) {
+        paddleX += 5;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if (leftPressed) {
+        paddleX -= 5;
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
 }
 
 setInterval(draw, 10);
